246cefb99835982916a4bc5939bf6bd8aaa7b340
viyaga <mohan2609kumar@gmail.com>
2025-07-29 21:35:48 +0530

    logo

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -7,13 +7,13 @@ app_color = "#e74c3c"
 app_email = "hello@frappe.io"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
+app_logo_url = "/assets/erpnext/images/viyaga-v-logo.svg"
 
 
 add_to_apps_screen = [
 	{
 		"name": "erpnext",
-		"logo": "/assets/erpnext/images/erpnext-logo-blue.png",
+		"logo": "/assets/erpnext/images/viyaga-v-logo-blue.png",
 		"title": "ERPNext",
 		"route": "/app/home",
 		"has_permission": "erpnext.check_app_permission",
@@ -109,8 +109,8 @@ calendars = ["Task", "Work Order", "Sales Order", "Holiday List", "ToDo"]
 website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
-	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"favicon": "/assets/erpnext/images/viyaga-favicon.svg",
+	"splash_image": "/assets/erpnext/images/viyaga-v-logo.svg",
 }
 
 # nosemgrep
